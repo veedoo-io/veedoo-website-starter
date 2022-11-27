a1586425166111ddeb8d2b030397322cd0ca50b9
taoufiq lotfi <leh.lotfi@gmail.com>
2022-11-27 10:06:01 +0100

    added text atom

---
+++ components/text/index.stories.js
@@ -0,0 +1,71 @@
+import MyComponent from './index.vue';
+
+export default {
+  title: 'atoms/Text',
+  component: MyComponent,
+};
+
+const Template = (args, { argTypes }) => ({
+  components: { MyComponent },
+  props: Object.keys(argTypes),
+  template: '<MyComponent v-bind="$props" />',
+});
+
+export const DefaultTextH1 = Template.bind({});
+
+DefaultTextH1.args = {
+  text: 'Text Atom',
+  htmlTag: 'h1',
+  class: 'text-blue-300 text-5xl',
+};
+
+export const TextWithStartIcon = Template.bind({});
+
+TextWithStartIcon.args = {
+  text: 'Tag Content',
+  htmlTag: 'h1',
+  class: 'text-blue-300 ',
+  startIcon:
+    'https://veedoo-web.cdn.prismic.io/veedoo-web/2c9e3f32-7c05-47b1-aaa3-45e4f1b118be_Logo+Icon.svg',
+};
+
+export const TextWithEndIcon = Template.bind({});
+
+TextWithEndIcon.args = {
+  text: 'Tag Content',
+  htmlTag: 'p',
+  class: 'text-blue-300 text-5xl',
+  endIcon:
+    'https://veedoo-web.cdn.prismic.io/veedoo-web/2c9e3f32-7c05-47b1-aaa3-45e4f1b118be_Logo+Icon.svg',
+};
+
+export const TextWithStartAndEndIcon = Template.bind({});
+
+TextWithStartAndEndIcon.args = {
+  text: 'Tag Content',
+  htmlTag: 'p',
+  class: 'text-blue-300 text-[150px]',
+  startIcon:
+    'https://veedoo-web.cdn.prismic.io/veedoo-web/2c9e3f32-7c05-47b1-aaa3-45e4f1b118be_Logo+Icon.svg',
+  endIcon:
+    'https://veedoo-web.cdn.prismic.io/veedoo-web/2c9e3f32-7c05-47b1-aaa3-45e4f1b118be_Logo+Icon.svg',
+  iconClass: 'w-32 h-32',
+};
+
+export const LinkText = Template.bind({});
+
+LinkText.args = {
+  text: 'Click Me I am a Link',
+  htmlTag: 'a',
+  class: 'text-blue-300 underline text-5xl',
+  url: 'https://veedoo.io',
+};
+
+export const TextWithEmoji = Template.bind({});
+
+TextWithEmoji.args = {
+  text: '🚀 I am a text with emoji 🚀',
+  htmlTag: 'h1',
+  class: 'text-red-300  text-5xl',
+  url: 'https://veedoo.io',
+};
---
+++ components/text/index.vue
@@ -0,0 +1,57 @@
+<script>
+import { h } from 'vue';
+
+export default {
+  props: {
+    text: { type: String, default: '' },
+    htmlTag: {
+      type: String,
+      required: true,
+      default: 'p',
+      validator(value) {
+        // The value must match one of these strings
+        return ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'].includes(value);
+      },
+    },
+    class: { type: String, default: '' },
+    startIcon: { type: String, default: '' },
+    endIcon: { type: String, default: '' },
+    url: { type: String, default: '' },
+    iconClass: { type: String, default: 'h-[1em] w-[1em]' },
+  },
+  render(h) {
+    return h({
+      template: `
+
+
+  <${this.htmlTag} ${
+        this.url != '' ? "href='" + this.url + "'" : ''
+      } class="flex items-center justify-center w-fit h-fit gap-2  ${
+        this.class
+      }" >${
+        this.startIcon != ''
+          ? "<img class='" +
+            this.iconClass +
+            "'  src='" +
+            this.startIcon +
+            "' alt='Start Icon' />"
+          : ''
+      }
+      ${this.text}
+      ${
+        this.endIcon != ''
+          ? "<img class='" +
+            this.iconClass +
+            "'  src='" +
+            this.endIcon +
+            "' alt='Start Icon' />"
+          : ''
+      }
+      </${this.htmlTag}>
+
+
+`,
+    });
+  },
+};
+</script>
