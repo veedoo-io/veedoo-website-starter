4e5725454b8d2839b26135b8000b6d08cf277e52
taoufiq lotfi <leh.lotfi@gmail.com>
2023-01-02 18:13:52 +0100

    Rename RichTextAdvanced

--- .slicemachine/assets/slices/RichTextAdvanced/index.stories.js
+++ .slicemachine/assets/slices/RichTextAdvanced/index.stories.js
@@ -1,26 +1,57 @@
-import MyComponent from '../../../../slices/RichText';
-import SliceZone from 'vue-slicezone'
+import MyComponent from '../../../../slices/RichTextAdvanced';
+import SliceZone from 'vue-slicezone';
 
 export default {
-  title: 'slices/RichText'
-}
-
+  title: 'slices/RichTextAdvanced',
+};
 
 export const _Default = () => ({
   components: {
     MyComponent,
-    SliceZone
+    SliceZone,
   },
   methods: {
     resolve() {
-      return MyComponent
-    }
+      return MyComponent;
+    },
   },
   data() {
     return {
-      mock: {"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"htmltag":"h1","text":"lovely","startimage":{"dimensions":{"width":900,"height":500},"alt":null,"copyright":null,"url":"https://images.unsplash.com/photo-1600804931749-2da4ce26c869"},"end_image":{"dimensions":{"width":900,"height":500},"alt":null,"copyright":null,"url":"https://images.unsplash.com/photo-1499951360447-b19be8fe80f5"},"url":{"link_type":"Web","url":"https://slicemachine.dev"},"background_color":"#5e4521","text_color":"#ecb61b","text_size":73,"slice_type":"free Mode","font_weight":"normal","margin_top":33,"margin_bottom":73,"margin_left":87,"margin_right":81},"slice_type":"text","id":"_Default"}
-    }
+      mock: {
+        variation: 'default',
+        version: 'sktwi1xtmkfgx8626',
+        items: [{}],
+        primary: {
+          htmltag: 'h1',
+          text: 'lovely',
+          startimage: {
+            dimensions: { width: 900, height: 500 },
+            alt: null,
+            copyright: null,
+            url: 'https://images.unsplash.com/photo-1600804931749-2da4ce26c869',
+          },
+          end_image: {
+            dimensions: { width: 900, height: 500 },
+            alt: null,
+            copyright: null,
+            url: 'https://images.unsplash.com/photo-1499951360447-b19be8fe80f5',
+          },
+          url: { link_type: 'Web', url: 'https://slicemachine.dev' },
+          background_color: '#5e4521',
+          text_color: '#ecb61b',
+          text_size: 73,
+          slice_type: 'free Mode',
+          font_weight: 'normal',
+          margin_top: 33,
+          margin_bottom: 73,
+          margin_left: 87,
+          margin_right: 81,
+        },
+        slice_type: 'text',
+        id: '_Default',
+      },
+    };
   },
-  template: '<SliceZone :slices="[mock]" :resolver="resolve" />'
-})
-_Default.storyName = ''
+  template: '<SliceZone :slices="[mock]" :resolver="resolve" />',
+});
+_Default.storyName = '';
