69b22b944b2d9f9b60d0319d4380a377a590d084
taoufiq lotfi <leh.lotfi@gmail.com>
2023-01-30 14:58:20 +0100

    added storybook to run dev concurrently config

--- .slicemachine/assets/slices/FooterVeedoo/index.stories.js
+++ .slicemachine/assets/slices/FooterVeedoo/index.stories.js
@@ -1,26 +1,57 @@
 import MyComponent from '../../../../slices/FooterVeedoo';
-import SliceZone from 'vue-slicezone'
+import SliceZone from 'vue-slicezone';
 
 export default {
-  title: 'slices/FooterVeedoo'
-}
-
+  title: 'Footers/FooterVeedoo',
+};
 
 export const _Default = () => ({
   components: {
     MyComponent,
-    SliceZone
+    SliceZone,
   },
   methods: {
     resolve() {
-      return MyComponent
-    }
+      return MyComponent;
+    },
   },
   data() {
     return {
-      mock: {"variation":"default","version":"sktwi1xtmkfgx8626","items":[{"footer_flag":{"dimensions":{"width":900,"height":500},"alt":null,"copyright":null,"url":"https://images.unsplash.com/photo-1587614295999-6c1c13675117"}}],"primary":{"footer_logo":{"dimensions":{"width":900,"height":500},"alt":null,"copyright":null,"url":"https://images.unsplash.com/photo-1545239351-1141bd82e8a6"},"footer_text":[{"type":"paragraph","text":"Incididunt aliquip officia sit et aute ipsum est ipsum.","spans":[]}],"background_color":"#3152de","text_color":"#24ed56"},"slice_type":"footer_veedoo","id":"_Default"}
-    }
+      mock: {
+        variation: 'default',
+        version: 'sktwi1xtmkfgx8626',
+        items: [
+          {
+            footer_flag: {
+              dimensions: { width: 900, height: 500 },
+              alt: null,
+              copyright: null,
+              url: 'https://images.unsplash.com/photo-1587614295999-6c1c13675117',
+            },
+          },
+        ],
+        primary: {
+          footer_logo: {
+            dimensions: { width: 900, height: 500 },
+            alt: null,
+            copyright: null,
+            url: 'https://images.unsplash.com/photo-1545239351-1141bd82e8a6',
+          },
+          footer_text: [
+            {
+              type: 'paragraph',
+              text: 'Incididunt aliquip officia sit et aute ipsum est ipsum.',
+              spans: [],
+            },
+          ],
+          background_color: '#3152de',
+          text_color: '#24ed56',
+        },
+        slice_type: 'footer_veedoo',
+        id: '_Default',
+      },
+    };
   },
-  template: '<SliceZone :slices="[mock]" :resolver="resolve" />'
-})
-_Default.storyName = ''
+  template: '<SliceZone :slices="[mock]" :resolver="resolve" />',
+});
+_Default.storyName = '';
