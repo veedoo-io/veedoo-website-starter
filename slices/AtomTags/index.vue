ee85f455f0c89f1733dde6171e96d5dfa7a26f9c
monvika <v.monastyrska@ukr.net>
2023-07-24 09:12:47 +0200

    fix-carousel

--- slices/AtomTags/index.vue
+++ slices/AtomTags/index.vue
@@ -3,11 +3,7 @@
     :class="getContainerClasses()"
     :style="`padding:${marginTop} ${marginRight} ${marginBottom} ${marginLeft}`"
   >
-    <div
-      v-if="slice?.items?.length > 0"
-      v-for="(item, i) in slice.items"
-      :key="`slice-item-${i}`"
-    >
+    <div v-for="(item, i) in slice.items" :key="`slice-item-${i}`">
       <Tag
         :title="item.tag_label"
         :image="item.tag_image?.url"
@@ -18,36 +14,36 @@
 </template>
 
 <script>
-import { getSliceComponentProps } from '@prismicio/vue/components';
+import { getSliceComponentProps } from "@prismicio/vue/components";
 
 export default {
-  name: 'AtomTags',
+  name: "AtomTags",
   // The array passed to `getSliceComponentProps` is purely optional and acts as a visual hint for you
-  props: getSliceComponentProps(['slice', 'index', 'slices', 'context']),
+  props: getSliceComponentProps(["slice", "index", "slices", "context"]),
 
   data() {
     let marginTop =
       this.slice.primary.margin_top && this.slice.primary.margin_top >= 0
-        ? this.slice.primary.margin_top + 'px'
-        : '0px';
+        ? this.slice.primary.margin_top + "px"
+        : "0px";
 
     let marginBottom =
       this.slice.primary.margin_bottom && this.slice.primary.margin_bottom >= 0
-        ? this.slice.primary.margin_bottom + 'px'
-        : '0px';
+        ? this.slice.primary.margin_bottom + "px"
+        : "0px";
 
     let marginLeft =
       this.slice.primary.margin_left && this.slice.primary.margin_left >= 0
-        ? this.slice.primary.margin_left + 'px'
-        : '0px';
+        ? this.slice.primary.margin_left + "px"
+        : "0px";
 
     let marginRight =
       this.slice.primary.margin_right && this.slice.primary.margin_right >= 0
-        ? this.slice.primary.margin_right + 'px'
-        : '0px';
+        ? this.slice.primary.margin_right + "px"
+        : "0px";
 
     let getContainerClasses = function () {
-      let classes = 'flex gap-[32px] flex-wrap';
+      let classes = "flex gap-[32px] flex-wrap";
 
       return classes;
     };
