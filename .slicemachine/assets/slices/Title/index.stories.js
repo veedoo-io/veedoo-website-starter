5d5147d556b99dcb579a10c04eb990d062ca7fe0
monvika <v.monastyrska@ukr.net>
2023-08-02 14:57:11 +0200

    fix cards

--- .slicemachine/assets/slices/Title/index.stories.js
+++ .slicemachine/assets/slices/Title/index.stories.js
@@ -18,7 +18,7 @@ export const _Default = () => ({
   },
   data() {
     return {
-      mock: {"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"paragraph","text":"Irure incididunt consequat et.","spans":[]}],"text_alignment":"left","image":{"dimensions":{"width":900,"height":500},"alt":null,"copyright":null,"url":"https://images.unsplash.com/photo-1494173853739-c21f58b16055"},"text_color":"#cbf9b4","background_color":"#b79157","text_size":16,"margin_top":83,"margin_right":86,"margin_bottom":22,"margin_left":4},"slice_type":"title","id":"_Default"}
+      mock: {"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"paragraph","text":"Velit consequat sunt pariatur fugiat ex consequat cillum nisi eu fugiat.","spans":[]}],"text_alignment":"left","image":{"dimensions":{"width":3277,"height":4092},"alt":null,"copyright":null,"url":"https://images.unsplash.com/photo-1494173853739-c21f58b16055"},"text_color":"#624f7b","background_color":"#7b349e","text_size":46,"margin_top":50,"margin_right":43,"margin_bottom":62,"margin_left":33},"id":"_Default","slice_type":"title"}
     }
   },
   template: '<SliceZone :slices="[mock]" :resolver="resolve" />'
