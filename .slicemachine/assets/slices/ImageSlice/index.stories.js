2f43dd163ab75279cd906604d698cddd806371f7
monvika <v.monastyrska@ukr.net>
2023-09-21 12:50:42 +0200

    10px by default

--- .slicemachine/assets/slices/ImageSlice/index.stories.js
+++ .slicemachine/assets/slices/ImageSlice/index.stories.js
@@ -18,7 +18,7 @@ export const _Default = () => ({
   },
   data() {
     return {
-      mock: {"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"image":{"dimensions":{"width":3424,"height":3424},"alt":null,"copyright":null,"url":"https://images.unsplash.com/photo-1493119508027-2b584f234d6c"},"margin_top":5,"margin_bottom":23,"background_color":"#739dc2","rounded":"10","alignment":"center"},"id":"_Default","slice_type":"image"}
+      mock: {"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"image":{"dimensions":{"width":4000,"height":4000},"alt":null,"copyright":null,"url":"https://images.unsplash.com/photo-1591012911207-0dbac31f37da"},"margin_top":59,"margin_bottom":19,"background_color":"#5806ec","rounded":"10","alignment":"center"},"id":"_Default","slice_type":"image"}
     }
   },
   template: '<SliceZone :slices="[mock]" :resolver="resolve" />'
