c912093d1a2bc19431b1a20737c78975b7148ecc
monvika <v.monastyrska@ukr.net>
2024-01-19 11:58:45 +0100

    add images in two columns slice

--- .slicemachine/assets/slices/TwoColumnTextBlock/index.stories.js
+++ .slicemachine/assets/slices/TwoColumnTextBlock/index.stories.js
@@ -18,7 +18,7 @@ export const _Default = () => ({
   },
   data() {
     return {
-      mock: {"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"first_column_rich_text":[{"type":"paragraph","text":"Eu irure id mollit consequat consectetur laboris do ipsum proident aute velit cupidatat cupidatat anim. Ea ut consectetur dolore.","spans":[]}],"second_column_rich_text":[{"type":"paragraph","text":"Aute et culpa elit cillum in est minim.","spans":[]}],"text_color":"#0cb8db","background_color":"#57b9d1","margin_top":90,"margin_bottom":96},"id":"_Default","slice_type":"two_column_text_block"}
+      mock: {"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"first_column_rich_text":[{"type":"paragraph","text":"Ullamco Lorem aliquip labore cupidatat reprehenderit labore voluptate laborum nulla dolore occaecat ut.","spans":[]}],"second_column_rich_text":[{"type":"paragraph","text":"Aute ipsum labore tempor cupidatat aute consectetur anim Lorem ullamco anim id velit.","spans":[]}],"text_color":"#0cb8db","background_color":"#57b9d1","margin_top":90,"margin_bottom":96},"id":"_Default","slice_type":"two_column_text_block"}
     }
   },
   template: '<SliceZone :slices="[mock]" :resolver="resolve" />'
