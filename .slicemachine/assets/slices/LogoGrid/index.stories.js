5c9f6fcece04a4cc6f5dfc3cefbc34f70a1ebc9e
monvika <v.monastyrska@ukr.net>
2024-02-14 15:06:39 +0100

    link in grid

--- .slicemachine/assets/slices/LogoGrid/index.stories.js
+++ .slicemachine/assets/slices/LogoGrid/index.stories.js
@@ -18,7 +18,7 @@ export const _Default = () => ({
   },
   data() {
     return {
-      mock: {"variation":"default","version":"sktwi1xtmkfgx8626","items":[{"image":{"dimensions":{"width":4172,"height":4000},"alt":null,"copyright":null,"url":"https://images.unsplash.com/photo-1586952518485-11b180e92764"}}],"primary":{"margin_top":10,"margin_bottom":45},"id":"_Default","slice_type":"logo_grid"}
+      mock: {"variation":"default","version":"sktwi1xtmkfgx8626","items":[{"image":{"dimensions":{"width":4172,"height":4000},"alt":null,"copyright":null,"url":"https://images.unsplash.com/photo-1586952518485-11b180e92764"},"link":{"link_type":"Web","url":"http://google.com"}}],"primary":{"margin_top":10,"margin_bottom":45},"id":"_Default","slice_type":"logo_grid"}
     }
   },
   template: '<SliceZone :slices="[mock]" :resolver="resolve" />'
