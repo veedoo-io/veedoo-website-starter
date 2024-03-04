d02b4364545fc7e7cf9d0703dafc52cf7a465ea4
monvika <v.monastyrska@ukr.net>
2024-03-04 09:42:09 +0100

    twitter main page

--- pages/index.vue
+++ pages/index.vue
@@ -37,6 +37,11 @@ export default {
         property: "og:title",
         content: this.page?.data?.meta_title,
       });
+      meta.push({
+        hid: "twitter_title",
+        name: "twitter:title",
+        content: this.page?.data?.meta_title,
+      });
     }
 
     if (this.page?.data?.meta_description) {
@@ -50,11 +55,21 @@ export default {
         property: "og:description",
         content: this.page?.data?.meta_description,
       });
+      meta.push({
+        hid: "twitter_description",
+        name: "twitter:description",
+        content: this.page?.data?.meta_description,
+      });
     }
     if (this.page?.data?.meta_image?.url) {
       meta.push({
         hid: "og:image",
         property: "og:image",
+        content: this.page?.data?.meta_image?.url + "&fm=jpg&w=1200&h=630",
+      });
+      meta.push({
+        hid: "twitter_image",
+        property: "twitter:image",
         content: this.page?.data?.meta_image?.url + "&fm=jpg&w=1200&h=630",
       });
     }
